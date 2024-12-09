e054ea2df181d7207567fef57dd0863ead08cfc3
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-12-10 03:09:38 +0900

    refactor: clean up comments and streamline variable assignments in findShortestRoute function

--- src/models/routeModel.js
+++ src/models/routeModel.js
@@ -3,18 +3,14 @@ const buildGraph = require("../utils/buildGraph");
 const dijkstra = require("../utils/dijkstra");
 
 async function findShortestRoute(startGcd, endGcd) {
-	// Lấy dữ liệu
 	const [stations] = await pool.query("SELECT station_cd, station_g_cd, station_name FROM railway_stations");
 	const [connections] = await pool.query("SELECT station_cd1, station_cd2, line_cd FROM railway_line_connections");
 	const [lines] = await pool.query("SELECT line_cd, line_name FROM railway_lines");
 
-	// Xây dựng đồ thị dựa trên station_g_cd
 	const graphData = buildGraph(connections, stations);
 
-	// Chạy Dijkstra trên station_g_cd
 	const { path, totalDistance, transfers, previous } = dijkstra(graphData.graph, startGcd, endGcd);
 
-	// Lấy map groupedStations để tiện truy xuất
 	const groupedStations = graphData.groupedStations;
 
 	const route = [];
@@ -25,21 +21,16 @@ async function findShortestRoute(startGcd, endGcd) {
 		const currentGcd = path[i];
 		const nextGcd = path[i + 1];
 
-		// Tìm line dùng để đến nextGcd
 		const lineCd = previous[nextGcd]?.line;
 		if (!lineCd) {
-			// Ở node bắt đầu có thể chưa có previous line, trường hợp này có thể bỏ qua hoặc tìm line bằng cách khác.
-			// Tuy nhiên, thông thường previous[nextGcd].line sẽ có giá trị từ node thứ hai trở đi.
 			continue;
 		}
 
-		const currentCandidates = groupedStations[currentGcd]; // Mảng station_cd thuộc gcd hiện tại
-		const nextCandidates = groupedStations[nextGcd]; // Mảng station_cd thuộc gcd kế tiếp
-
+		const currentCandidates = groupedStations[currentGcd];
+		const nextCandidates = groupedStations[nextGcd];
 		let chosenCurrentCd = null;
 		let chosenNextCd = null;
 
-		// Tìm cặp station_cd thoả mãn lineCd và kết nối giữa currentGcd và nextGcd
 		outerLoop: for (const ccd of currentCandidates) {
 			for (const ncd of nextCandidates) {
 				const conn = connections.find(
@@ -56,12 +47,10 @@ async function findShortestRoute(startGcd, endGcd) {
 			}
 		}
 
-		// Lấy thông tin ga và line
 		const fromStation = stations.find((s) => s.station_cd === chosenCurrentCd);
 		const toStation = stations.find((s) => s.station_cd === chosenNextCd);
 		const lineObj = lines.find((l) => l.line_cd === lineCd);
 
-		// Thêm segment vào route
 		route.push({
 			from: chosenCurrentCd,
 			from_name: fromStation ? fromStation.station_name : null,
@@ -70,7 +59,6 @@ async function findShortestRoute(startGcd, endGcd) {
 			line: lineObj ? lineObj.line_name : null,
 		});
 
-		// Kiểm tra chuyển tuyến
 		if (previousLine && previousLine !== lineCd) {
 			const transferStation = fromStation || toStation;
 			detailedTransfers.push({

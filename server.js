fb260916c9de86fab4265dd81272924af696b20e
NGUYEN DUC HUYNH <duchuynhnguyen1906@gmail.com>
2024-12-09 15:58:55 +0900

    update import path for railwayRoutes to reflect new directory structure

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 });
 
 // Import routes
-const railwayRoutes = require("./routes/railwayRoutes");
+const railwayRoutes = require("./src/routes/railwayRoutes");
 app.use("/railways", railwayRoutes);
 
 // Start the server
